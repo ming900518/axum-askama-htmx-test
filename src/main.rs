47a5b28910e62e9cc8af01de15831a02c3fe2a6b
Ming Chang <mail@mingchang.tw>
2024-12-30 13:56:24 +0800

    repeat sent message, use Cookies, update logger.

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,51 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 use std::{collections::HashMap, convert::Infallible, net::SocketAddr, sync::OnceLock};
 
 mod types;
 
 use askama::Template;
 use axum::{
-    extract::Path,
     http::StatusCode,
     response::{
         sse::{Event, KeepAlive},
         Html, Sse,
     },
     routing::{get, post},
-    Form, Router, Server,
+    Form, Router,
 };
+use axum_extra::extract::{cookie::Cookie, CookieJar};
 use futures::Stream;
-use tokio::sync::{mpsc::Sender, RwLock};
+use time::macros::{format_description, offset};
+use tokio::{
+    net::TcpListener,
+    sync::{mpsc::Sender, RwLock},
+};
 use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 use tower_http::trace::TraceLayer;
 use tracing::Level;
-use tracing_subscriber::{filter, prelude::*};
+use tracing_subscriber::{filter, fmt::time::OffsetTime, prelude::*};
 use types::{ChatView, Index, SendMsgReq, SseView};
 
-
-static TX: OnceLock<RwLock<HashMap<usize, Sender<String>>>> = OnceLock::new();
+static TX: OnceLock<RwLock<HashMap<u16, Sender<String>>>> = OnceLock::new();
 
 #[tokio::main]
 async fn main() {
-    let tracing_filter = filter::Targets::new()
-        .with_target("tower_http::trace::on_response", Level::DEBUG)
-        .with_target("tower_http::trace::on_request", Level::DEBUG)
-        .with_target("tower_http::trace::make_span", Level::DEBUG)
-        .with_default(Level::INFO);
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer())
-        .with(tracing_filter)
-        .init();
-
+    init_logger();
     TX.set(RwLock::new(HashMap::new()))
         .expect("Unable to init TX");
 
     let router = Router::new()
         .route("/", get(index))
         .route("/start", post(start))
-        .route("/send_msg/:user_id", post(send_msg))
-        .route("/sse/:user_id", get(sse))
+        .route("/send_msg", post(send_msg))
+        .route("/sse", get(sse))
         .layer(TraceLayer::new_for_http())
         .into_make_service();
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 1370));
+    let listener = TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], 8000)))
+        .await
+        .expect("Unable to listen port 8000.");
 
-    Server::bind(&addr)
-        .serve(router)
+    axum::serve(listener, router)
         .await
         .expect("Server startup failed.");
 }
@@ -60,11 +54,11 @@ async fn index() -> Html<String> {
     Html::from(Index.render().unwrap_or_default())
 }
 
-async fn start() -> (StatusCode, Html<String>) {
-    let user_id = rand::random::<usize>();
+async fn start(cookies: CookieJar) -> (CookieJar, Html<String>) {
+    let user_id = rand::random::<u16>();
 
     (
-        StatusCode::OK,
+        cookies.add(Cookie::new("UserId", format!("{user_id}"))),
         Html::from(
             ChatView {
                 user_id: user_id.to_string(),
@@ -76,9 +70,17 @@ async fn start() -> (StatusCode, Html<String>) {
 }
 
 async fn sse(
-    Path(user_id): Path<usize>,
+    cookies: CookieJar,
 ) -> Result<Sse<impl Stream<Item = Result<Event, Infallible>>>, StatusCode> {
     let (tx, rx) = tokio::sync::mpsc::channel::<String>(1);
+
+    let Some(user_id) = cookies.get("UserId") else {
+        return Err(StatusCode::UNAUTHORIZED);
+    };
+
+    let Ok(user_id) = user_id.value().parse() else {
+        return Err(StatusCode::BAD_REQUEST);
+    };
 
     match TX.get() {
         Some(global_tx) => {
@@ -90,31 +92,71 @@ async fn sse(
     }
 }
 
-async fn send_msg(Path(user_id): Path<usize>, Form(req): Form<SendMsgReq>) -> StatusCode {
+async fn send_msg(cookies: CookieJar, Form(req): Form<SendMsgReq>) -> StatusCode {
     let Some(global_tx) = TX.get() else {
         return StatusCode::INTERNAL_SERVER_ERROR;
     };
 
-    if let (Some(_), Some(tx)) = (
-        global_tx.read().await.get(&user_id),
-        global_tx.read().await.get(&req.target_id),
-    ) {
-        let tx = tx.clone();
-        match tx
-            .send(
-                SseView {
-                    from_user_id: user_id,
-                    data: req.message,
-                }
-                .render()
-                .unwrap_or_default(),
-            )
-            .await
-        {
-            Ok(()) => StatusCode::OK,
-            Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    let Some(user_id) = cookies.get("UserId") else {
+        return StatusCode::UNAUTHORIZED;
+    };
+
+    let Ok(user_id) = user_id.value().parse() else {
+        return StatusCode::BAD_REQUEST;
+    };
+
+    let reader = global_tx.read().await;
+    if let (Some(self_tx), Some(target_tx)) = (reader.get(&user_id), reader.get(&req.target_id)) {
+        match (
+            self_tx
+                .send(
+                    SseView {
+                        from_user_id: user_id,
+                        data: req.message.clone(),
+                    }
+                    .render()
+                    .unwrap_or_default(),
+                )
+                .await,
+            target_tx
+                .send(
+                    SseView {
+                        from_user_id: user_id,
+                        data: req.message,
+                    }
+                    .render()
+                    .unwrap_or_default(),
+                )
+                .await,
+        ) {
+            (Ok(()), Ok(())) => StatusCode::OK,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
         }
     } else {
         StatusCode::NOT_FOUND
     }
+}
+
+fn init_logger() {
+    let format = tracing_subscriber::fmt::format()
+            .compact()
+            .with_timer(OffsetTime::new(
+                offset!(+8),
+                format_description!("[year]-[month]-[day]T[hour]:[minute]:[second][offset_hour sign:mandatory]:[offset_minute padding:zero]"),
+            ))
+            .with_target(cfg!(debug_assertions))
+            .with_thread_names(cfg!(debug_assertions))
+            .with_source_location(cfg!(debug_assertions))
+            .with_line_number(cfg!(debug_assertions));
+
+    let filter = filter::Targets::new()
+        .with_target("tower_http::trace::on_response", Level::DEBUG)
+        .with_target("tower_http::trace::on_request", Level::DEBUG)
+        .with_target("tower_http::trace::make_span", Level::DEBUG)
+        .with_default(Level::INFO);
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().event_format(format))
+        .with(filter)
+        .init();
 }
--- src/types.rs
+++ src/types.rs
@@ -14,12 +14,12 @@ pub struct ChatView {
 #[derive(Template)]
 #[template(path = "sse.html")]
 pub struct SseView {
-    pub from_user_id: usize,
+    pub from_user_id: u16,
     pub data: String,
 }
 
 #[derive(Deserialize)]
 pub struct SendMsgReq {
-    pub target_id: usize,
+    pub target_id: u16,
     pub message: String,
 }
